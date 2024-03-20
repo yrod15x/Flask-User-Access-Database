b944ffd9e87d5ffe3166b7f23607d50c6770972d
yrod15x <megasoldier15x@hotmail.com>
2024-03-20 10:20:55 -0400

    Formularion WTForms terminados.

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
+from formularios import FormAcceso, FormRegistro
 
 app = Flask(__name__)
 annio = datetime.now().year
+
+app.config['SECRET_KEY'] = 'ABC1234'
 
 @app.route('/')
 def index():
     return render_template('index.html', titulo = "User's Page", titulo_banner = "Home", annio = annio)
 
-@app.route('/registrar')
+@app.route('/registrar', methods = ['POST', 'GET'])
 def registrar():
-    return render_template('registro.html', titulo = "User's Page", titulo_banner = "Registration", annio = annio)
+    #Instancia de clase que permite usar los elementos del formulario
+    formulario = FormRegistro()
+    #Si el usuario escribio bien los campos lo regresa a la pagina de inicio
+    if formulario.validate_on_submit():
+        print('A casa')
+        return redirect(url_for('index'))
+    return render_template('registro.html', titulo = "User's Page", titulo_banner = "Registration", annio = annio, formulario = formulario)
 
 @app.route('/acceder')
 def acceder():
-    return render_template('acceso.html', titulo = "User's Page", titulo_banner = "Loggin In", annio = annio)
+    formulario = FormAcceso()
+    return render_template('acceso.html', titulo = "User's Page", titulo_banner = "Loggin In", annio = annio, formulario = formulario)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ formularios.py
@@ -0,0 +1,20 @@
+#Permite la conversión de codigo python en html
+from flask_wtf import FlaskForm
+#Permite agregar los elementos (etiquetas) de un formulario
+from wtforms import StringField, PasswordField, SubmitField
+#Permite agregar validaciones a las etiquetas del formulario
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+import email_validator
+
+class FormRegistro(FlaskForm):
+    """Cada formulario se hace en base a una casa para su posterior instanciamiento en los archivos HTML. Estas clases heredan las variables y metodos de la clas FlaskForm"""
+    nom_usuario = StringField('Nombre Usuario', validators = [DataRequired(), Length(min=4, max= 20)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    contrasena = PasswordField('Contraseña', validators = [DataRequired()])
+    rep_contrasena = PasswordField('Confirme Contraseña', validators=[DataRequired(), EqualTo(contrasena)])
+    btn_registrar = SubmitField('Registrar')
+    
+class FormAcceso(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    contrasena = PasswordField('Contraseña', validators = [DataRequired()])
+    btn_acceder = SubmitField('Acceder')
